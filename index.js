0f0d5833e738c59d84e96d7838396ae368faf96c
prajwal <prajwalshah912@gmail.com>
2023-06-22 23:43:54 +0530

    element child in dom

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-alert("external js")
+let check = document.querySelector("input").click()
+
+//properties
+// ex: innerHTML, style, firstChild
+//methods
+//ex: click(), appendChild(), setAttribute()
+
+
+// document.firstElementChild;  --  all the html file
+// document.firstElementChild.firstElementChild;  --  goes inside html and find the first ele that is <head />
+// document.firstElementChild.lastElementChild;  --  goes inside html and find the last ele that is <body />
+// document.firstElementChild.lastElementChild.firstElementChild;  --  goes inside html and find the last ele that is <body /> goes inside and select the h1
+
+
+let ul = document.querySelector("ul").lastElementChild;
+ul.innerHTML = "prajwal"; 
+//  --  select the ul last li and change the text to prajwal
+
+
+
